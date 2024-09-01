605519309c8670eb7d406dc960e1a3ddbfc1116b
Jinseonghyun <jinsunghyun9@gmail.com>
2024-09-01 16:28:10 +0900

    캐쉬 활성화

--- src/main/java/jin/dataredis/DataRedisApplication.java
+++ src/main/java/jin/dataredis/DataRedisApplication.java
@@ -2,8 +2,10 @@ package jin.dataredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // 캐쉬 활성화
 public class DataRedisApplication {
 
     public static void main(String[] args) {
